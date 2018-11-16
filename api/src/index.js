c3fb6f97af7ea76d8461a1361b839eaa409530cd
Weihong Guan <weihong.guan@gmail.com>
2018-11-16 18:37:23 +0800

    logger and config extension

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,42 @@
 import express from 'express';
+import expressWinston from 'express-winston';
+import { createLogger, format, transports } from 'winston';
+import config from 'config';
 
-const app = express();
-const port = 8000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`); // eslint-disable-line no-console
+const logger = createLogger({
+  format: format.combine(
+    format.timestamp(),
+    format.json(),
+  ),
+  transports: [
+    new transports.Console({ level: config.get('logLevel') }),
+  ],
 });
+
+const initApp = () => new Promise((resolve) => {
+  const app = express();
+  app.use(expressWinston.logger({
+    winstonInstance: logger,
+    level: 'verbose',
+  }));
+
+  app.get('/', (req, res) => res.json({
+    result: 'Hello World!',
+  }));
+
+  resolve(app);
+});
+
+
+if (['development', 'production'].includes(process.env.NODE_ENV)) {
+  (async () => {
+    const { PORT = 8000 } = process.env;
+    try {
+      const app = await initApp();
+      app.listen(PORT, () => logger.info(`Example app listening on port ${PORT}!`));
+    } catch (err) {
+      logger.warn(err.stack);
+      process.exit(1);
+    }
+  })();
+}
